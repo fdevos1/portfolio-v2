8c1244ce664c822710af283fba352e5995d50c06
fdevos1 <felipe.devosm@gmail.com>
2023-10-04 23:04:53 -0300

    change next build

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { useTranslations } from "next-intl";
 import Layout from "../../components/layout";
 import About from "../../components/sections/about";
 import Contact from "../../components/sections/contact";
 import Hero from "../../components/sections/hero";
 import Jobs from "../../components/sections/jobs";
 import Projects from "../../components/sections/projects";
-import { useRouter } from "next/router";
 
 export default function Home() {
-  const { isFallback } = useRouter();
-
-  if (isFallback) {
-    return <h1>Data loading</h1>;
-  }
-
   return (
     <div>
       <Layout>
